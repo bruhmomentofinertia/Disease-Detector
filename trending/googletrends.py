095fa6f9e4fecde4252f281927f9f947d566f26d
soso <80224791+disco-io@users.noreply.github.com>
2023-03-05 20:58:51 -0500

    Update googletrends.py

--- trending/googletrends.py
+++ trending/googletrends.py
@@ -2,13 +2,12 @@ def graph_trends(symptom, areaCode):
   from pytrends.request import TrendReq
   import matplotlib.pyplot as plt
 
-  # Create pytrends object and specify region and time period
+  # create pytrends object and specify region and time period
   pytrends = TrendReq(hl='en-US', tz=360)
   pytrends.build_payload(kw_list=[symptom], timeframe='today 5-y',  geo='US-VA-' + str(areaCode))
   # Request data and extract values
   data = pytrends.interest_over_time().values[:, 0]
 
-  # Plot data using matplotlib
   plt.plot(range(len(data)), data)
   plt.xlabel('Week')
   plt.ylabel('Search Trend')
